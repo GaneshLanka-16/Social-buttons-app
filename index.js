d27f2e4d50b3480cec79f286f707c9bc01f6b830
GaneshLanka-16 <ganeshlanka16@gmail.com>
2026-05-09 15:40:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const Button = props => {
+  const {className, buttonText} = props
+  return <button className={`${className}`}>{buttonText} </button>
+}
+
+const element = (
+  <div>
+    <h1>Social Button </h1>
+    <div className='buttonContainer'>
+      <Button buttonText='Like' className='yellow' />
+      <Button buttonTexte='Comment' className='white' />
+      <Button buttonText='Share' className='blue' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
